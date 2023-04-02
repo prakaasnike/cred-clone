770e6fae79987665dfb8e5240629a2bb245351e7
Prakash Nayak <erprakashnayak@gmail.com>
2023-04-02 20:01:45 +0545

    productshowcase image fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import Button from './Button'
-import { ArrowDownLeftIcon } from '@heroicons/react/24/solid'
+import React from 'react';
+import Button from './Button';
+import { ArrowDownLeftIcon } from '@heroicons/react/24/solid';
 const Hero = () => {
-
     return (
-        <div className=''>
-            {/* <div className='flex-row justify-center flex gap-4 py-6 bg-gray-600 bg-opacity-30 items-center'>
+        <div className="">
+            <div className="flex-row justify-center flex gap-4 py-6 bg-gray-600 bg-opacity-30 items-center">
                 <div>pay credit card bill.earn guranteed ₹200 back.</div>
-                <div className='gap-2 flex cursor-pointer text-blue-400'>
+                <div className="gap-2 flex cursor-pointer text-blue-400">
                     <span>claim now</span>
-                    <span className='rotate-180'>
+                    <span className="rotate-180">
                         <ArrowDownLeftIcon className="w-5 h-5" />
                     </span>
                 </div>
-            </div> */}
-            <div className='max-w-7xl h-[70vh]  items-center mx-auto justify-center md:text-center flex flex-col px-10'>
-                <div className='space-y-9 md:pl-20 md:pr-20'>
-                    <div className='text-[55px] font-extrabold md:text-6xl lg:text-[106px] leading-[55px]  tracking-[-1.65px] md:tracking-[-3px] md:leading-[120px]'>rewards for paying credit card bills.</div>
-                    <div className='font-semibold md:font-bold text-lg md:text-3xl'>join 9M+ members who win rewards and cashbacks everyday</div>
+            </div>
+            <div className="max-w-7xl h-[70vh]  items-center mx-auto justify-center md:text-center flex flex-col px-10">
+                <div className="space-y-9 md:pl-20 md:pr-20">
+                    <div className="text-[55px] font-extrabold md:text-6xl lg:text-[106px] leading-[55px]  tracking-[-1.65px] md:tracking-[-3px] md:leading-[120px]">
+                        rewards for paying credit card bills.
+                    </div>
+                    <div className="font-semibold md:font-bold text-lg md:text-3xl">
+                        join 9M+ members who win rewards and cashbacks everyday
+                    </div>
                     <Button buttonText="Download CRED" />
                 </div>
             </div>
-
-
-
         </div>
-    )
-}
+    );
+};
 
-export default Hero
+export default Hero;
--- src/pages/HomePageSection/ProductShowCase.jsx
+++ src/pages/HomePageSection/ProductShowCase.jsx
@@ -1,13 +1,17 @@
-import React, { useEffect, useRef, useState } from 'react'
-import ShowcaseBG from '../../assets/images/product-showcase/video-bg.webp'
-import clsx from 'clsx'
+import React, { useEffect, useRef, useState } from 'react';
+import LeftImage1 from '../../assets/images/product-showcase/neopop-left-1.webp';
+import LeftImage2 from '../../assets/images/product-showcase/neopop-left-2.webp';
+import CenterImage from '../../assets/images/product-showcase/neopop-center.webp';
+import RightImage1 from '../../assets/images/product-showcase/neopop-right-1.webp';
+import RightImage2 from '../../assets/images/product-showcase/neopop-right-2.webp';
+import clsx from 'clsx';
 
 const ProductShowCase = () => {
-    const [showAnimation, setShowAnimation] = useState(false)
-    const ref = useRef(null)
+    const [showAnimation, setShowAnimation] = useState(false);
+    const ref = useRef(null);
 
     const toggleAnimation = (entries) => {
-        entries.forEach(entry => {
+        entries.forEach((entry) => {
             if (entry.isIntersecting) {
                 setShowAnimation(true);
             }
@@ -16,36 +20,63 @@ const ProductShowCase = () => {
 
     const options = {
         root: null,
-        rootMargin: "0px",
+        rootMargin: '0px',
         threshold: 0.5,
-    }
+    };
 
     useEffect(() => {
         const observer = new IntersectionObserver(toggleAnimation, options);
         if (!showAnimation) {
             if (ref.current) {
-                observer.observe(ref.current)
+                observer.observe(ref.current);
             }
         }
 
         return () => {
             if (ref.current) {
-                observer.unobserve(ref.current)
+                observer.unobserve(ref.current);
             }
-        }
-    }, [showAnimation, options])
+        };
+    }, [showAnimation, options]);
 
     return (
-        <div ref={ref} className={clsx(`product-showcase w-screen  md:h-[800px]  relative overflow-hidden bg-cover bg-right ${showAnimation ? 'scale-in-bottom' : ""} `)}>
+        <div
+            ref={ref}
+            className={clsx(
+                `product-showcase w-screen  md:h-[800px]  relative overflow-hidden bg-cover bg-right ${
+                    showAnimation ? 'scale-in-bottom' : ''
+                } `
+            )}
+        >
             <div className="showcase-wrapper flex">
-                <img className='showcase-ui showcase-mockup-1 -bottom-[400px] md:left-[-800px] absolute inset-0 m-auto' src="/src/assets/images/product-showcase/neopop-left-1.webp" alt="" />
-                <img className='showcase-ui showcase-mockup-2 -bottom-[250px] md:left-[-500px] absolute inset-0 m-auto' src="/src/assets/images/product-showcase/neopop-left-2.webp" alt="" />
-                <img className='showcase-ui showcase-mockup-3 -bottom-[100px] absolute inset-0 m-auto z-10' src="/src/assets/images/product-showcase/neopop-center.webp" alt="" />
-                <img className='showcase-ui showcase-mockup-5 -bottom-[400px] md:right-[-800px] absolute inset-0 m-auto' src="/src/assets/images/product-showcase/neopop-right-2.webp" alt="" />
-                <img className='showcase-ui showcase-mockup-4 -bottom-[250px] md:right-[-500px] absolute inset-0 m-auto' src="/src/assets/images/product-showcase/neopop-right-1.webp" alt="" />
+                <img
+                    className="showcase-ui showcase-mockup-1 -bottom-[400px] md:left-[-800px] absolute inset-0 m-auto"
+                    src={LeftImage1}
+                    alt=""
+                />
+                <img
+                    className="showcase-ui showcase-mockup-2 -bottom-[250px] md:left-[-500px] absolute inset-0 m-auto"
+                    src={LeftImage2}
+                    alt=""
+                />
+                <img
+                    className="showcase-ui showcase-mockup-3 -bottom-[100px] absolute inset-0 m-auto z-10"
+                    src={CenterImage}
+                    alt=""
+                />
+                <img
+                    className="showcase-ui showcase-mockup-5 -bottom-[400px] md:right-[-800px] absolute inset-0 m-auto"
+                    src={RightImage1}
+                    alt=""
+                />
+                <img
+                    className="showcase-ui showcase-mockup-4 -bottom-[250px] md:right-[-500px] absolute inset-0 m-auto"
+                    src={RightImage2}
+                    alt=""
+                />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default ProductShowCase
+export default ProductShowCase;
